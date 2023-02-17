e6be265d1d197975e70a7b4b23f2e9250d33446b
treid003 <31581841+treid003@users.noreply.github.com>
2023-02-17 00:55:00 -0500

    Add more example files for generating random values

---
+++ random_values.js
@@ -0,0 +1,37 @@
+//Generate Random Numbers
+///Math.random()
+document.getElementById("math.random").innerHTML = Math.random();
+
+///crypto.getRandomValues()
+a = new Uint8Array(1), window.crypto.getRandomValues(a);
+document.getElementById("crypto.getRandomValues").innerHTML = a[0];
+
+// Generate safeframe URL 
+///Reference: https://securepubads.g.doubleclick.net/gpt/pubads_impl_2023010901.js?cb=31071543
+////Archived Version: https://web.archive.org/web/20230113005605/https://securepubads.g.doubleclick.net/gpt/pubads_impl_2023010901.js?cb=31071543
+
+///Math.random()
+////Create the random number array (code from Th())
+var a;
+a = Array(16);
+for (b = 0; b < a.length; b++) 
+    a[b] = Math.floor(255 * Math.random());
+////Convert the array to a hexadecimal string (similar to what is done in Uh())
+randomValue = "";
+for(b=0; b < a.length; b++)
+    c = a[b], 15 >= c && (randomValue += "0"), randomValue += c.toString(16); 
+safeframeURL = "https://" + randomValue + ".safeframe.googlesyndication.com" + "/safeframe/1-0-40/html/container.html?n=1";
+document.getElementById("math.random_Safeframe").href = safeframeURL;
+document.getElementById("math.random_Safeframe").innerHTML = safeframeURL;
+
+///crypto.getRandomValues()
+////Create the random number array (code from Th())
+var a;
+a = new Uint8Array(16), window.crypto.getRandomValues(a);
+////Convert the array to a hexadecimal string (similar to what is done in Uh())
+randomValue = "";
+for(b=0; b < a.length; b++)
+    c = a[b], 15 >= c && (randomValue += "0"), randomValue += c.toString(16); 
+safeframeURL = "https://" + randomValue + ".safeframe.googlesyndication.com" + "/safeframe/1-0-40/html/container.html?n=1";
+document.getElementById("crypto.getRandomValues_Safeframe").href = safeframeURL;
+document.getElementById("crypto.getRandomValues_Safeframe").innerHTML = safeframeURL;
